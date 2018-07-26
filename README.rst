544cfb3bb41b79aa67acd58fadde1ff7671d2ec0
Paul Colomiets <paul@colomiets.name>
2018-07-26 16:44:01 +0300

    Upgrade description

--- README.rst
+++ README.rst
@@ -3,8 +3,8 @@ Cantal-Py
 =========
 
 :Status: Beta
-:Documentation: http://cantal-py.readthedocs.org/
-:Parent Project: git://github.com/tailhook/cantal
+:Documentation: https://cantal-py.readthedocs.io/
+:Parent Project: https://github.com/tailhook/cantal
 
 Cantal is an expermimental heartbeating, monitoring and statistics solution.
 This is a python library for sending statistics data to cantal.
--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@
 
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), 'rt') as f:
+    long_description = f.read()
+
+
 setup(name='cantal',
       version='0.2.2',
       description='The library that submits metrics to cantal',
@@ -13,11 +19,12 @@ setup(name='cantal',
       classifiers=[
           'Development Status :: 4 - Beta',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
       ],
+      long_description=long_description,
       )
